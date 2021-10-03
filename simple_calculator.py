62eb58e2cce4b20dad5a0578a84c2e0bb517339d
recongamer <72912524+recongamer@users.noreply.github.com>
2021-10-03 14:41:56 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,11 +1,11 @@
 #python program - make simple calculator
 
-print("1. adiition")
+print("1. addition")
 print("2. subtraction")
 print("3. multiplication")
 print("4. division")
 print("5. exit")
-choice = int(input("enter your choice: "))
+choice = int(input("Enter your choice: "))
 if (choice>=1 and choice<=4):
     print("enter two numbers: ")
     num1 = int(input())
@@ -15,17 +15,17 @@ if (choice>=1 and choice<=4):
         print("result = ", res)
     elif choice == 2:
             res = num1 - num2
-            print("result=",res)
+            print("result = ",res)
     elif choice==3:
             res=num1*num2
-            print("result=",res)
+            print("result = ",res)
     else:
         res=num1/num2
-        print("result=",res)
+        print("result = ",res)
 elif choice==5:
     exit()
 else:
-    print("wrong input..!!")
+    print("Invalid input!\nPlease try again")
             
             
     
