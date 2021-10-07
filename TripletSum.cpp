474071378393108344ddcfb5803ab58bc47e9882
khushi-0001 <92074294+khushi-0001@users.noreply.github.com>
2021-10-07 23:57:58 +0530

    Update TripletSum.cpp

--- TripletSum.cpp
+++ TripletSum.cpp
@@ -1,3 +1,8 @@
+//You have been given a random integer array/list(ARR) and a number X.
+//Find and return the number of triplets in the array/list which sum to X.
+
+
+
 int tripletSum(int *input, int size, int x)
 {
     int count=0;
