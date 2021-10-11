42943ab8cda4556c0857fb0156393ddc45771d1a
Aryan <aryangangwar000@gmail.com>
2021-10-11 10:02:33 +0530

    Note-Making-App-Kotlin by @aryangangwar

---
+++ Android Projects/Note-Making-App-Kotlin/app/src/main/java/com/aryan/notemaking/NoteRoomDatabase.kt
@@ -0,0 +1,33 @@
+package com.aryan.notemaking
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Notes::class], version = 1, exportSchema = false)
+public abstract class NoteRoomDatabase: RoomDatabase(){
+    abstract fun getNoteDao(): NoteDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NoteRoomDatabase? = null
+
+        fun getDatabase(context: Context): NoteRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteRoomDatabase::class.java,
+                    "note_database"
+                ).build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+}
