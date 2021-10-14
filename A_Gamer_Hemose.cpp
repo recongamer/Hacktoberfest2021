70eba463ede0c447fdb9d7635e31ded9d491f5d4
33hm <91753013+33hm@users.noreply.github.com>
2021-10-14 12:36:07 +0530

    Add files via upload

---
+++ A_Gamer_Hemose.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize ("trapv")
+#include <bits/stdc++.h>
+#include<algorithm>
+#include <vector>
+#include<cstring>
+#include<cstdlib>
+#include<cmath>
+#include<cstdlib>
+#include<sstream>
+#include <string.h>
+using namespace std;
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define  rep(i,n,v) for(i=n;i<v;i++)
+#define per(i,n,v) for(i=n;i>v;i--)
+#define ff first 
+#define ss second 
+#define pp pair<ll,ll>
+#define ll  long long
+#define ld  long double
+#define endl "\n"
+ 
+const ll mod=1e9+7;
+void solve()
+{
+ll  n, a=0,b=0,m=1, c=-1,k=0, i=0, j=0, l=1e9+5;
+string s,p, q;
+    cin>>n>>a;
+    ll ar[n];
+    rep(i,0,n) cin>>ar[i];
+    sort(ar,ar+n);
+    b=ar[n-1];m=ar[n-2];
+    c=a/(b+m);
+    k+=2*c;
+    if(c)
+    a=a%(b+m);
+    if(a>0){
+        
+        c=a/b;
+        if(c) {a=a%b;
+        k++;}
+        if(a>0) 
+        {
+            k++;
+        }
+    }
+    cout<<k<<endl;
+}
+int main()
+{
+ios_base::sync_with_stdio(false);
+cin. tie(0);cout. tie(0);
+ll t=1;
+cin>>t;
+while(t--)
+{
+solve();
+}
+return 0;
+}
